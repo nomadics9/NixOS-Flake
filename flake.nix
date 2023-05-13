54db395fb6604047f72d36eee7fe71355d0ced71
nomadics9 <nomadics9>
2023-05-14 01:47:15 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My Simple NixOS";
+
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+
+  outputs = { self, nixpkgs }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+       inherit system;
+       config.allowUnfree = true;
+        };
+        lib = nixpkgs.lib;
+    in {
+        nixosConfigurations = {
+          sager = lib.nixosSystem {
+              inherit system;
+              modules = [ ./configuration.nix ];
+            };
+          };
+      };
+  }
+
