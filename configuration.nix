879794c7721dad6ca15f0169ffcfb298e0491386
nomadics9 <binaskar9@mgail.com>
2023-05-31 02:50:43 +0300

    bash aliases

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,8 @@
 
 
 programs.bash.shellAliases = {
+  switch = "sudo nixos-rebuild switch --flake .#nomad";
+  switchu = "sudo nixos-rebuild switch --upgrade --flake .#nomad";
   clean = "sudo nix-collect-garbage -d";
   cleanold = "sudo nix-collect-garbage --delete-old";
   cleanboot = "sudo /run/current-system/bin/switch-to-configuration boot";
